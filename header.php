48ab4feeb3528c7ebd1a647582f0406b5f8d8050
Josh Armentano <josh@abidewebdesign.com>
2020-10-09 15:58:26 -0700

    Added favicon.

--- header.php
+++ header.php
@@ -17,6 +17,7 @@ defined( 'ABSPATH' ) || exit;
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png" />
 	<?php wp_head(); ?>
 </head>
 
